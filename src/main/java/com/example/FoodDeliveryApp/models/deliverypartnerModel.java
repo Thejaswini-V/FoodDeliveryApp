cf242ea7a74cc86143859fec93635a216653796b
Thejaswini-V <thejaswini.v.aids2021@citchennai.net>
2024-07-15 22:07:17 +0530

    models commit

---
+++ src/main/java/com/example/FoodDeliveryApp/models/deliverypartnerModel.java
@@ -0,0 +1,20 @@
+package com.example.FoodDeliveryApp.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Delivery_partner")
+public class deliverypartnerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private int deliveryId;
+
+    @Column(name = "delivery_phn")
+    private long deliveryPhn;
+
+    @Column(name = "order_id")
+    private int orderId;
+}
